a96cefeb3e2d858cb2f6e91e205f20b3c5ea4467
Cedric Mathis <clmathis@uh.edu>
2019-06-25 09:33:24 -0500

    commit Node modules

--- Server.js
+++ Server.js
@@ -1,14 +1,7 @@
-const http = require('http');
+import express from 'express';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
