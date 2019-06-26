0c56bd7bd966f91deddbd7b17889ec267c442e29
Cedric Mathis <clmathis@uh.edu>
2019-06-26 10:39:58 -0500

    Set up both login and registration pages with routing

--- Server.js
+++ Server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import path from 'path';
+import router from './Routes/index.js';
 
 let app = express();
 
-// Direct User to when the first go to the localhost.. direct them to the login page to login or register
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/User/index.html'));
-});
+// Set up Pathway for static files -- HTML/CSS/JS
+app.use(express.static(path.join(__dirname,'Public')));
+
+//Set up a pathway for all of or routes in our route folder
+app.use(router);
 
 //Set Port Number and run server with yarn run start
 let port = 3000;
