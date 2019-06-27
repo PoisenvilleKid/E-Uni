618183c6f1f1669cd882aa59b10a5c2c3427b544
Cedric Mathis <clmathis@uh.edu>
2019-06-27 12:13:47 -0500

    Setting Up Service Layer

--- Models/index.js
+++ Models/index.js
@@ -4,23 +4,16 @@ import User from './User.model';
 import Event from './Event.model';
 import { Server } from 'https';
 
-
-
 // Set up the connection to our DB if there is an error
 // Let the user know wether or not our connection is valid or not
 mongoose.connect('mongodb://localhost:27017/E-Uni',{ useNewUrlParser: true })
    .then(() => console.log("MongoDB Connected..."))
    .catch(err => console.log(err))
   
-
   /* Set up array of all of our models so we can export 
      them in one call in Server.js
   */   
   const models = {User, Event};
-
-  /* Export the Connection to the DB in one call in Server.js
-     export{connectDB};
-  */
 
   // Export our models so the program can use universally
   export default models;
